30f2d5f6ee1c9301e015e88d3cb2d4b28ad27c7a
Josh Zappone <zappone.josh@gmail.com>
2022-12-01 20:25:02 -0800

    Day 1

---
+++ 2022/day1/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	elfBag []int
+	numA   int
+	numB   int
+	count  = 1
+	elfId  int
+)
+
+func check(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, "Oh sh**. Don't Panic:", e)
+	}
+}
+
+func main() {
+
+	file, err := os.Open("input.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) > 0 {
+			num, err := strconv.Atoi(line)
+			check(err)
+			elfBag = append(elfBag, num)
+		} else {
+			fmt.Println("Elf complete. Add slice \n", elfBag)
+			for _, v := range elfBag {
+				numB += v
+			}
+			if numB > numA {
+				fmt.Println("Elf Wins!")
+				numA = numB
+				elfId = count
+			}
+			numB = 0
+
+			count++
+			elfBag = nil
+		}
+	}
+	fmt.Printf("The elf carrying the most calroies is elf: %d\n", elfId)
+	fmt.Printf("They are carrying %d calories\n", numA)
+	err = scanner.Err()
+	check(err)
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Advent of Code Challenges
+
+_Is it too optimistic to have a year directory at the top level?_
+
+_I only did one day in 2021._
+
+https://adventofcode.com
