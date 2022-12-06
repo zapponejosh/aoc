0c75d55b4d7ce386f107c36b71f7b6fccf1a7e5e
Josh Zappone <zappone.josh@gmail.com>
2022-12-05 22:26:23 -0800

    day3 part 1

--- 2022/day3/main.go
+++ 2022/day3/main.go
@@ -13,6 +13,39 @@ func check(e error) {
 	}
 }
 
+func findCommonItems(stringSacks ...string) string {
+	// sackCount := len(stringSacks)
+	itemMap := make(map[string]int)
+	// loop over each string set
+	for i, strs := range stringSacks {
+		var unique string
+		for _, v := range strs {
+			if !strings.Contains(unique, string(v)) {
+				unique = unique + string(v)
+			}
+		}
+		for _, itm := range unique {
+			v, _ := itemMap[string(itm)]
+
+			// if it's already in there we need to continue to the next string
+			// but if it's there from a previous string we need to keep going!
+			// what if i de-dup every string first?
+			if v < i+1 {
+				itemMap[string(itm)]++
+			}
+
+		}
+	}
+	// iterate over map
+	// var commonItem string
+	for v := range itemMap {
+		if itemMap[v] == 3 {
+			return v
+		}
+	}
+	return "NOOOO"
+}
+
 func part1(file *os.File) {
 	var prioritySum int
 	priority := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -42,6 +75,33 @@ func part1(file *os.File) {
 	err := scanner.Err()
 	check(err)
 }
+func part2(file *os.File) {
+	var prioritySum int
+	priority := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	scanner := bufio.NewScanner(file)
+	var count = 1
+	var elves []string
+
+	for scanner.Scan() {
+		elf := scanner.Text()
+		if count < 3 {
+			elves = append(elves, elf)
+			count++
+		} else if count == 3 {
+			elves = append(elves, elf)
+			commonItem := findCommonItems(elves...)
+			ItmPriority := strings.Index(priority, commonItem)
+			prioritySum += (ItmPriority + 1)
+			fmt.Println(commonItem)
+			count = 1
+			elves = nil
+		}
+	}
+	fmt.Println(prioritySum)
+	// fmt.Println(prioritySum)
+	err := scanner.Err()
+	check(err)
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -49,6 +109,7 @@ func main() {
 	defer file.Close()
 
 	part1(file)
-	// file.Seek(0, 0)
-	// part2(file)
+	file.Seek(0, 0)
+	part2(file)
+
 }
